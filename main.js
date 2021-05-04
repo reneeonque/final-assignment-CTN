9fa171ae98772db78446b33ea4b7082f39b53b42
Renee Onque <reneeonque@Karimas-iMac.home>
2021-05-04 19:09:49 -0400

    Filter Fixed

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const select_menu = document.querySelector(".filter");
+	const select_menus = document.querySelectorAll(".filter");
 
-	select_menu.addEventListener("change", function (e) {
-		const year_class = e.target.value;
-		const active_items = document.querySelectorAll(".item.active");
 
-		active_items.forEach(function (item) {
-			item.classList.remove("active");
-		});
+	select_menus.forEach(function (select_menu) {
+		select_menu.addEventListener("change", function (e) {
+			const year_class = e.target.value;
+			const filter_container = e.target.closest(".filter-container");
+			const active_items = filter_container.querySelectorAll(".item.active");
 
-		const year_items = document.querySelectorAll(`.item${year_class}`);
-		year_items.forEach(function (item) {
-			item.classList.add("active");
-		});
-	});
-});
+			active_items.forEach(function (item) {
+				item.classList.remove("active");
+			});
+
+			console.log(year_class);
+
+			const year_items = filter_container.querySelectorAll(`.item.${year_class}`);
+			year_items.forEach(function (item) {
+				item.classList.add("active");
+			});
+		}); //end listener
+
+	}); //ends loop
+}); //ends DOM Ready
